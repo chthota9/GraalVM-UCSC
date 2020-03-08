34443e87d694f0e2f3ab21843d8759fc0ab93fc8
gusperson <40843503+gusperson@users.noreply.github.com>
2020-03-07 17:03:42 -0800

    removed comments

--- IntelliJPlugin/src/main/java/actionJavaPackage/Factory.java
+++ IntelliJPlugin/src/main/java/actionJavaPackage/Factory.java
@@ -1,18 +1,16 @@
-package actionJavaPackage;
+package ActionPackage;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-
 //import com.intellij.openapi.project.Project;
 //import com.intellij.openapi.wm.ToolWindow;
 //import com.intellij.openapi.wm.ToolWindowEP;
 //import com.intellij.openapi.wm.ToolWindowFactory;
 //import com.intellij.ui.content.Content;
 //import com.intellij.ui.content.ContentFactory;
-
 import javax.swing.*;
 
 class Factory implements ToolWindowFactory {
@@ -26,9 +24,6 @@ class Factory implements ToolWindowFactory {
         newWindow.setSize(300,300);
         newWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel textLabel = new JLabel ("hello world", JLabel.CENTER);
-//        newWindow.getContentPane().add (textLabel, JLabel.CENTER);
-//        newWindow.setVisible(true);
-
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(textLabel,"", false);
         toolWindow.getContentManager().addContent(content);
